a2f4c84e703c08bf2134913da8644feb0839a086
王志邦 <edwardmartin927@gmail.com>
2022-12-25 21:32:27 +0800

    121 买卖股票的最佳时机

---
+++ codetop/dynamicprogram/121-best-time-to-buy-and-sell-stock.go
@@ -0,0 +1,23 @@
+package dynamicprogram
+
+func maxProfit(prices []int) int {
+	maxP := 0
+	buy := prices[0]
+
+	max := func(a, b int) int {
+		if a > b {
+			return a
+		}
+		return b
+	}
+
+	for _, price := range prices {
+		if price > buy {
+			maxP = max(price-buy, maxP)
+		} else {
+			buy = price
+		}
+	}
+
+	return maxP
+}
