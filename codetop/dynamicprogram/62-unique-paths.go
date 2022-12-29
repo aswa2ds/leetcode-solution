dfc4c4a38975bc2029ed188483f533107496fd6a
aswa2ds <Edwardmartin927>
2022-12-29 17:45:20 +0800

    62 不同路径 组合算法

--- codetop/dynamicprogram/62-unique-paths.go
+++ codetop/dynamicprogram/62-unique-paths.go
@@ -1,15 +1,22 @@
 package dynamicprogram
 
+import "math/big"
+
 func uniquePaths(m int, n int) int {
-	dp := make([]int, n+1)
-	dp[0] = 1
-
-	for i := 0; i < m; i++ {
-		for j := 0; j < n; j++ {
-			dp[j+1] = dp[j] + dp[j+1]
-		}
-		dp[0] = 0
-	}
-
-	return dp[len(dp)-1]
+	return int(new(big.Int).Binomial(int64(m+n-2), int64(n-1)).Int64())
 }
+
+// 动态规划
+//func uniquePaths(m int, n int) int {
+//	dp := make([]int, n+1)
+//	dp[0] = 1
+//
+//	for i := 0; i < m; i++ {
+//		for j := 0; j < n; j++ {
+//			dp[j+1] = dp[j] + dp[j+1]
+//		}
+//		dp[0] = 0
+//	}
+//
+//	return dp[len(dp)-1]
+//}
