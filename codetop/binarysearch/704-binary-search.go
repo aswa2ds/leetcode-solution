94ba2c361263f5adc110b668feb8f1f5074ae63e
aswa2ds <Edwardmartin927>
2023-01-04 09:35:17 +0800

    704 二分查找

---
+++ codetop/binarysearch/704-binary-search.go
@@ -0,0 +1,25 @@
+package binarysearch
+
+func search(nums []int, target int) int {
+	n := len(nums)
+
+	lo, hi := 0, n-1
+
+	for lo < hi {
+		mid := (lo + hi) >> 1
+		if nums[mid] == target {
+			lo, hi = mid, mid
+			break
+		} else if nums[mid] < target {
+			lo = mid + 1
+		} else {
+			hi = mid
+		}
+	}
+
+	if nums[lo] == target {
+		return lo
+	}
+
+	return -1
+}
